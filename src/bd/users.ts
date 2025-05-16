8b04e49294a8694992e4026a630ac6158005b3ec
DavidHenrique24 <henriquemaciasdavidalejandro@fpllefia.com>
2025-05-16 17:45:53 +0200

    Acomodando Despliegue

---
+++ src/bd/users.ts
@@ -0,0 +1,12 @@
+export const users = [
+  { email: "mario@example.com", password: "1234" },
+  { email: "luigi@example.com", password: "5678" },
+  { email: "peach@example.com", password: "abcd" },
+  { email: "toad@example.com", password: "123456" },
+  { email: "yoshi@example.com", password: "123456" },
+  { email: "bowser@example.com", password: "123456" },
+  { email: "wario@example.com", password: "123456" },
+  { email: "daisy@example.com", password: "123456" },
+  { email: "rosalina@example.com", password: "123456" },
+  { email: "donkeykong@example.com", password: "123456" },
+]
